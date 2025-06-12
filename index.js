9e775677c957ca23b956287c73beb18c251c66cf
Nathashava <nataliavalla19@gmail.com>
2025-06-12 18:15:08 -0300

    examen1

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Examen de natalia valla")
+console.log("resto 20 - 5");
+
+let numero1 = 20
+let numero2 = 5
+let resta = numero1 - numero2
+console.log("la resta es de:", resta);
+
+console.log("sumo 100 + 40");
+let numero3 = 100
+let numero4 = 40
+let suma = numero3 + numero4
+console.log("la suma es de:", suma);
+
